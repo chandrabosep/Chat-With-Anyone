b52ddb28ea7aba0d6f6a50ad0c6ee3de3731fe64
Chandra Bose <chandrabosep3112@gmail.com>
2023-10-22 20:17:28 +0530

    added chatinput, CreateChat

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,7 +1,11 @@
 "use client"
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useSession } from 'next-auth/react';
 import {useForm} from 'react-hook-form';
 import * as z from 'zod';
+import { Button } from './ui/button';
+import { Form, FormControl, FormField, FormItem, FormMessage } from './ui/form';
+import { Input } from './ui/input';
 
 
 const formSchema = z.object({
@@ -16,11 +20,39 @@ function ChatInput({childId}: {childId: string}) {
             input: "",
         },
     });
+    async function onSubmit (formData: z.infer<typeof formSchema>){
+      if (values.input.length === 0){
+        return;
+      }
+      if(!session?.user){
+        return;
+      }
+    }
+
+    const userToStore
 
 
   return (
-    <div>
-      hey
+    <div className='sticky bottom-0'>
+      <Form {...form} />
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='flex space-x-2 p-2 rounded-t-xl max-w-4xl bg-white border dark:bg-slate-800'
+      >
+        <FormField
+        control={form.control}
+        name="input"
+        render={({ field }) => (
+          <FormItem className='flex-1'>
+            <FormControl>
+              <Input className='border-none bg-transparent dark:placeholder:text-white/70' placeholder='Enter message in ANY language...' {...field} />
+            </FormControl>
+            <FormMessage/>
+          </FormItem>
+        )}
+        />
+        <Button type='submit' className='bg-[#7775d6]'>Send</Button>
+      </form>
     </div>
   )
 }
